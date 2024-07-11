8963dd15fbc8ddf12fb7834ec678a80e7e8a2d9f
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-10 23:45:05 -0500

    feat: Add script to install Homebrew formulae and casks

---
+++ brew-apps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+formulae=(
+  "bat"
+  "btop"
+  "cmatrix"
+  "fastfetch"
+  "gh"
+  "go"
+  "lsd"
+  "neovim"
+  "trash"
+  "wget"
+)
+
+casks=(
+  "alacritty"
+  "alfred"
+  "appcleaner"
+  "authy"
+  "bitwarden"
+  "discord"
+  "firefox"
+  "google-chrome"
+  "imageoptim"
+  "keyboard-maestro"
+  "mos"
+  "orbstack"
+  "pika"
+  "rectangle"
+  "shottr"
+  "slack"
+  "tableplus"
+  "visual-studio-code"
+)
+
+for cask in "${casks[@]}"; do
+  echo "Installing $cask..."
+  brew install --cask "$cask"
+done
+
+echo "Casks installation complete."
+
+for formula in "${formulae[@]}"; do
+  echo "Installing $formula..."
+  brew install "$formula"
+done
+
+echo "Formulae installation complete."
