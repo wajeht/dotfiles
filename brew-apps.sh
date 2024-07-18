0e427df2c8f89750e675e20e228b923cd1cbe87e
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-18 17:30:26 -0500

    feat: Add installation functions for formulae and casks in brew script

--- brew-apps.sh
+++ brew-apps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found. Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  eval "$(/opt/homebrew/bin/brew shellenv)"  # For Apple Silicon
+  eval "$(/usr/local/bin/brew shellenv)"     # For macOS Intel
+fi
+
 formulae=(
   "bat"
   "btop"
@@ -39,16 +47,40 @@ casks=(
   "visual-studio-code"
 )
 
-for cask in "${casks[@]}"; do
-  echo "Installing $cask..."
-  brew install --cask "$cask"
-done
+install_casks() {
+  for cask in "${casks[@]}"; do
+    if ! brew list --cask --versions "$cask" &>/dev/null; then
+      echo "Installing $cask..."
+      if ! brew install --cask "$cask"; then
+        echo "Error installing $cask"
+      fi
+    else
+      echo "$cask is already installed."
+    fi
+  done
+  echo "Casks installation complete."
+}
 
-echo "Casks installation complete."
+install_formulae() {
+  for formula in "${formulae[@]}"; do
+    if ! brew list --versions "$formula" &>/dev/null; then
+      echo "Installing $formula..."
+      if ! brew install "$formula"; then
+        echo "Error installing $formula"
+      fi
+    else
+      echo "$formula is already installed."
+    fi
+  done
+  echo "Formulae installation complete."
+}
 
-for formula in "${formulae[@]}"; do
-  echo "Installing $formula..."
-  brew install "$formula"
-done
+brew update
 
-echo "Formulae installation complete."
+install_casks
+
+install_formulae
+
+brew cleanup
+
+echo "Installation and cleanup complete."
