f693d6d934fa0c2331ce1610a5ab573c43061d13
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-15 02:51:21 -0600

    feat(nvim): Add telescope plugin for enhanced fuzzy finding and file navigation

---
+++ .config/nvim/lua/wajeht/plugins/telescope.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "master",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				cond = vim.fn.executable("cmake") == 1,
+			},
+		},
+		config = function()
+			local actions = require("telescope.actions")
+
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+							["<C-x>"] = actions.delete_buffer,
+						},
+					},
+					file_ignore_patterns = {
+						"node_modules",
+						"vendor",
+						"yarn.lock",
+						".git",
+						".sl",
+						"_build",
+						".next",
+					},
+					hidden = true,
+					path_display = {
+						"filename_first",
+					},
+				},
+			})
+
+			-- Enable telescope fzf native, if installed
+			pcall(require("telescope").load_extension, "fzf")
+
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		end,
+	},
+}
--- .config/nvim/lua/wajeht/plugins/treesitter.lua
+++ .config/nvim/lua/wajeht/plugins/treesitter.lua
@@ -2,9 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
+  dependencies = {},
   config = function()
     -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
