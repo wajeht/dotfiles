327bed62a3989c3fc48899309b3e8bc06149c5e2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-15 03:56:03 -0600

    style(keymaps): Improve key mappings clarity and consistency

--- .config/nvim/lua/wajeht/core/keymaps.lua
+++ .config/nvim/lua/wajeht/core/keymaps.lua
@@ -8,29 +8,41 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- Save and Select All
-keymap.set("n", "<D-s>", ":w!<CR>", { desc = "Save file (force write)" }) -- Cmd+s in normal mode
-keymap.set("i", "<D-s>", "<Esc>:w!<CR>a", { desc = "Save file (force write) in insert mode" }) -- Cmd+s in insert mode
-keymap.set("n", "<D-a>", "ggVG", { desc = "Select all" }) -- Cmd+a to select all text in normal mode
+keymap.set("n", "<D-s>", ":wa!<CR>", { desc = "Save all files (force write)" })
+keymap.set("i", "<D-s>", "<Esc>:wa!<CR>a", { desc = "Save all files in insert mode" })
+keymap.set("n", "<D-a>", "ggVG", { desc = "Select all text in the buffer" })
 
 -- Window Management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equalize split sizes" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- Window Navigation
-keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Focus window to the left" })
+keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Focus window to the right" })
+keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Focus window below" })
+keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Focus window above" })
 
 -- Tab Management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open a new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close the current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Switch to the next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Switch to the previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in a new tab" })
 
--- Quick File Navigation (netrw file explorer on the right)
-keymap.set("n", "<leader>e", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer (right side)" }) -- Leader+e
-keymap.set("n", "<D-b>", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer (right side)" }) -- Cmd+b
+-- File Navigation
+keymap.set("n", "<leader>e", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer on the right" })
+keymap.set("n", "<D-b>", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer [Cmd+B]" })
+
+-- Buffers
+keymap.set("n", "<leader>'", "<C-^>", { desc = "Switch to the last used buffer" })
+
+-- Save
+keymap.set("n", "<leader>w", "<cmd>wa!<CR>", { desc = "Save all open files" })
+
+-- Quit
+keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit the current window" })
+
+-- Save and Quit
+keymap.set("n", "<leader>z", "<cmd>wa! | q<CR>", { desc = "Save all files and quit the current window" })
