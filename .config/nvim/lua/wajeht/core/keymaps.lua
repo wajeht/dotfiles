a1c2f582f2bef7f97c5d4aaf166d3bc7891dd505
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-15 04:37:09 -0600

    style(keymaps): Update key mappings for better navigation and centering

--- .config/nvim/lua/wajeht/core/keymaps.lua
+++ .config/nvim/lua/wajeht/core/keymaps.lua
@@ -41,3 +41,25 @@ vim.keymap.set("n", "<leader>w", "<cmd>wa!<CR>", { desc = "Save all files" })
 -- Move lines in visual mode
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
+
+-- Join lines without moving the cursor
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines without losing cursor position" })
+
+-- Paste without modifying the clipboard in visual mode
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without modifying the clipboard" })
+
+-- Centering the buffer while navigating
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "{", "{zz", { desc = "Go to previous block and center" })
+vim.keymap.set("n", "}", "}zz", { desc = "Go to next block and center" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Search next and center" })
+vim.keymap.set("n", "n", "nzz", { desc = "Search previous and center" })
+vim.keymap.set("n", "G", "Gzz", { desc = "Go to the end of the file and center" })
+vim.keymap.set("n", "gg", "ggzz", { desc = "Go to the start of the file and center" })
+vim.keymap.set("n", "gd", "gdzz", { desc = "Go to definition and center" })
+vim.keymap.set("n", "<C-i>", "<C-i>zz", { desc = "Go to the jump list forward and center" })
+vim.keymap.set("n", "<C-o>", "<C-o>zz", { desc = "Go to the jump list backward and center" })
+vim.keymap.set("n", "%", "%zz", { desc = "Jump to matching parenthesis and center" })
+vim.keymap.set("n", "*", "*zz", { desc = "Search for word under cursor and center" })
+vim.keymap.set("n", "#", "#zz", { desc = "Search backward for word under cursor and center" })
