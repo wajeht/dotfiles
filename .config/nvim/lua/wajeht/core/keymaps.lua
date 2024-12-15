72d2f5d49a26824f48707f7d241ed48325c94aef
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-15 04:20:02 -0600

    style: Improve keybindings and comments clarity in keymaps.lua

--- .config/nvim/lua/wajeht/core/keymaps.lua
+++ .config/nvim/lua/wajeht/core/keymaps.lua
@@ -2,47 +2,42 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
-local keymap = vim.keymap -- for conciseness
-
--- General
-keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+-- General Keymaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- Save and Select All
-keymap.set("n", "<D-s>", ":wa!<CR>", { desc = "Save all files (force write)" })
-keymap.set("i", "<D-s>", "<Esc>:wa!<CR>a", { desc = "Save all files in insert mode" })
-keymap.set("n", "<D-a>", "ggVG", { desc = "Select all text in the buffer" })
+vim.keymap.set("n", "<D-s>", ":w!<CR>", { desc = "Save file (force write)" }) -- Cmd+s in normal mode
+vim.keymap.set("i", "<D-s>", "<Esc>:w!<CR>a", { desc = "Save file (force write) in insert mode" }) -- Cmd+s in insert mode
+vim.keymap.set("n", "<D-a>", "ggVG", { desc = "Select all" }) -- Cmd+a to select all text in normal mode
 
 -- Window Management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equalize split sizes" })
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- Window Navigation
-keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Focus window to the left" })
-keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Focus window to the right" })
-keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Focus window below" })
-keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Focus window above" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Tab Management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open a new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close the current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Switch to the next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Switch to the previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in a new tab" })
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- File Navigation
-keymap.set("n", "<leader>e", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer on the right" })
-keymap.set("n", "<D-b>", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer [Cmd+B]" })
+-- Quick File Navigation (netrw file explorer on the right)
+vim.keymap.set("n", "<leader>e", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer (right side)" }) -- Leader+e
+vim.keymap.set("n", "<D-b>", "<cmd>vertical rightbelow Lex 30<CR>", { desc = "Open file explorer (right side)" }) -- Cmd+b
 
--- Buffers
-keymap.set("n", "<leader>'", "<C-^>", { desc = "Switch to the last used buffer" })
+-- Save and Quit (with leader)
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit Vim" })
+vim.keymap.set("n", "<leader>z", "<cmd>wq<CR>", { desc = "Save and Quit" })
+vim.keymap.set("n", "<leader>w", "<cmd>wa!<CR>", { desc = "Save all files" })
 
--- Save
-keymap.set("n", "<leader>w", "<cmd>wa!<CR>", { desc = "Save all open files" })
-
--- Quit
-keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit the current window" })
-
--- Save and Quit
-keymap.set("n", "<leader>z", "<cmd>wa! | q<CR>", { desc = "Save all files and quit the current window" })
+-- Move lines in visual mode
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
