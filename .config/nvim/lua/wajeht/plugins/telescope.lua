2eeddd780beea90d31a3ac6409b216b0167642a9
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-15 23:58:59 -0600

    feat(telescope.lua): Add keymap for fuzzy finding git files

--- .config/nvim/lua/wajeht/plugins/telescope.lua
+++ .config/nvim/lua/wajeht/plugins/telescope.lua
@@ -45,6 +45,7 @@ return {
       -- set keymaps
       local keymap = vim.keymap -- for conciseness
 
+      keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files" })
       keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
       keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
       keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
