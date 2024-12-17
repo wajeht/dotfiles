0f232d3d242d7a5ac0f00aefb7596962af9a0ff0
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-17 11:06:12 -0600

    refactor: Update colorscheme.lua with new colorscheme and dependencies

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -1,7 +1,9 @@
 return {
-  "Mofiqul/vscode.nvim",
+ "rockyzhang24/arctic.nvim",
+  branch = "v2",
+  dependencies = { "rktjmp/lush.nvim" },
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme vscode")
+    vim.cmd("colorscheme arctic")
   end,
 }
