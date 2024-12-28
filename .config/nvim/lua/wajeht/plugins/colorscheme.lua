0921479505d7dbd1946006092d6ae022faec0b4c
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-27 21:26:17 -0600

    feat(config): Add Ghostty configuration and update Neovim colorscheme plugin

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
+ "rockyzhang24/arctic.nvim",
+  branch = "v2",
+  dependencies = { "rktjmp/lush.nvim" },
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+    vim.cmd("colorscheme arctic")
+  end,
 }
