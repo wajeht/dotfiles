4177d6a72b58384477e272184b2a7c8bd8f158ad
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-27 12:18:01 -0600

    style(vim): Comment out unnecessary code in autocmds.lua for trailing whitespace

--- .config/nvim/lua/wajeht/core/autocmds.lua
+++ .config/nvim/lua/wajeht/core/autocmds.lua
@@ -13,15 +13,15 @@ vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold", "CursorHol
 })
 
 -- Auto-remove trailing whitespace on save
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = "*",
-  callback = function()
-      local save_cursor = vim.fn.getpos(".")
-      vim.cmd([[%s/\s\+$//e]])
-      vim.fn.setpos(".", save_cursor)
-  end,
-  desc = "Remove trailing whitespace on save"
-})
+-- vim.api.nvim_create_autocmd({"BufWritePre"}, {
+--   pattern = "*",
+--   callback = function()
+--       local save_cursor = vim.fn.getpos(".")
+--       vim.cmd([[%s/\s\+$//e]])
+--       vim.fn.setpos(".", save_cursor)
+--   end,
+--   desc = "Remove trailing whitespace on save"
+-- })
 
 -- Highlight yanked text briefly
 vim.api.nvim_create_autocmd({"TextYankPost"}, {
