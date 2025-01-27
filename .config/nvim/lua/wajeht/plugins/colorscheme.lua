a7dee97ffcb4263f5bd2c2a02cbaf1b096690c9f
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-27 13:32:13 -0600

    style: Update code formatting in options.lua and colorscheme.lua files

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -1,15 +1,36 @@
 return {
- "rockyzhang24/arctic.nvim",
-  branch = "v2",
-  dependencies = { "rktjmp/lush.nvim" },
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme arctic")
-    -- Custom statusline for terminal buffers
-    vim.cmd("set laststatus=0")  -- Disable default statusline
-    vim.cmd("set statusline=%{repeat('─',winwidth('.'))}")  -- Custom statusline (only a line of dashes)
+	{
+    "rockyzhang24/arctic.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    name = "arctic",
+    branch = "v2",
+    priority = 1000,
+    lazy = false,
+		config = function()
+			vim.cmd("colorscheme arctic")
 
-    vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#333333")  -- GUI background transparent
-    vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#333333")  -- Inactive StatusLine transparent for GUI
-  end,
+      -- Custom statusline for terminal buffers
+      vim.cmd("set laststatus=0")  -- Disable default statusline
+      vim.cmd("set statusline=%{repeat('─',winwidth('.'))}")  -- Custom statusline (only a line of dashes)
+
+      vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#333333")  -- GUI background transparent
+      vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#333333")  -- Inactive StatusLine transparent for GUI
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		priority = 1000,
+    priority = 1000,
+	},
 }
