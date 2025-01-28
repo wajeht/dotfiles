93adf8c36e0e59187033c36cae23c31b15b0a12e
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-27 22:39:00 -0600

    style(config): Update window padding and indentation in config file

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		name = "nightfox",
-		priority = 1000,
-		lazy = false,
-		config = function()
-			vim.cmd("colorscheme Terafox")
+    {
+        "EdenEast/nightfox.nvim",
+        name = "nightfox",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            vim.cmd("colorscheme Terafox")
 
-			-- Custom statusline for terminal buffers
-			vim.cmd("set laststatus=0") -- Disable default statusline
-			vim.cmd("set statusline=%{repeat('─',winwidth('.'))}") -- Custom statusline (only a line of dashes)
+            -- Custom statusline for terminal buffers
+            vim.cmd("set laststatus=0") -- Disable default statusline
+            vim.cmd("set statusline=%{repeat('─',winwidth('.'))}") -- Custom statusline (only a line of dashes)
 
-			vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#2d4f56") -- GUI background transparent
-			vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#2d4f56") -- Inactive StatusLine transparent for GUI
-		end,
-	},
-	-- {
-	-- 	"rockyzhang24/arctic.nvim",
-	-- 	dependencies = { "rktjmp/lush.nvim" },
-	-- 	name = "arctic",
-	-- 	branch = "v2",
-	-- },
+            vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#2d4f56") -- GUI background transparent
+            vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#2d4f56") -- Inactive StatusLine transparent for GUI
+        end,
+    },
+    -- {
+    -- 	"rockyzhang24/arctic.nvim",
+    -- 	dependencies = { "rktjmp/lush.nvim" },
+    -- 	name = "arctic",
+    -- 	branch = "v2",
+    -- },
 }
