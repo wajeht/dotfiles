bf6a0ebf00479c02328835f6748d3141bf9ccdc7
wajeht <58354193+wajeht@users.noreply.github.com>
2025-03-03 22:51:38 -0600

    feat: Add configuration for indent-blankline plugin

---
+++ .config/nvim/lua/wajeht/plugins/indent-blankline.lua
@@ -0,0 +1,20 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  main = "ibl",
+  config = function()
+    require("ibl").setup {
+      indent = { char = "┊" },
+      whitespace = {
+        remove_blankline_trail = false,
+      },
+      scope = {
+        enabled = true,
+        char = "┊", -- Same as regular indentation
+        show_start = false,
+        show_end = false,
+        priority = 1, -- Same priority as regular indentation
+      },
+    }
+  end,
+}
