37eaca04cdf36a18d922318ef5f5b8abae99fa68
wajeht <58354193+wajeht@users.noreply.github.com>
2025-03-23 15:10:23 -0500

    style: Update width of nvim-tree window to 50 in Lua configuration

--- .config/nvim/lua/wajeht/plugins/nvim-tree.lua
+++ .config/nvim/lua/wajeht/plugins/nvim-tree.lua
@@ -12,7 +12,7 @@ return {
                 },
                 view = {
                     side = "right", -- Open on the right side
-                    width = 30,     -- Set the width of the nvim-tree window
+                    width = 50,     -- Set the width of the nvim-tree window
                 },
                 renderer = {
                     indent_markers = {
