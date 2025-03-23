29e1113da260a4b0240e009a12e7eb8747a5b1cb
wajeht <58354193+wajeht@users.noreply.github.com>
2025-03-23 14:47:41 -0500

    style(config): Update theme and background color in Ghostty config and neovim plugins file

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -20,22 +20,22 @@
 -- }
 
 return {
-    {
-        "folke/tokyonight.nvim",
-        name = "tokyonight",
-        priority = 1000,
-        lazy = false,
-        config = function()
-            vim.cmd("colorscheme tokyonight-night")
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			vim.cmd("colorscheme tokyonight-night")
 
-            -- Custom statusline for terminal buffers
-            vim.cmd("set laststatus=0") -- Disable default statusline
-            vim.cmd("set statusline=%{repeat('─',winwidth('.'))}") -- Custom statusline (only a line of dashes)
+			-- Custom statusline for terminal buffers
+			vim.cmd("set laststatus=0") -- Disable default statusline
+			vim.cmd("set statusline=%{repeat('─',winwidth('.'))}") -- Custom statusline (only a line of dashes)
 
-            -- vim.cmd("hi StatusLine gui=NONE guibg=#NONE guifg=#3c425f") -- GUI background transparent
-            -- vim.cmd("hi StatusLineNC gui=NONE guibg=#NONE guifg=#3c425f") -- Inactive StatusLine transparent for GUI
-        end,
-    },
+			vim.cmd("hi StatusLine gui=NONE guibg=#16161d guifg=#16161d") -- GUI background transparent
+			vim.cmd("hi StatusLineNC gui=NONE guibg=#16161d guifg=#16161d") -- Inactive StatusLine transparent for GUI
+		end,
+	},
 }
 
 -- return {
