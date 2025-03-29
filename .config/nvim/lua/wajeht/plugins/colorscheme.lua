9ce150315df7fdb0a19fec8851ea3cabe52f7797
wajeht <58354193+wajeht@users.noreply.github.com>
2025-03-28 22:23:49 -0500

    style(config): Update theme to "dark modern" and adjust background colors

--- .config/nvim/lua/wajeht/plugins/colorscheme.lua
+++ .config/nvim/lua/wajeht/plugins/colorscheme.lua
@@ -1,36 +1,39 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"Mofiqul/vscode.nvim",
+		name = "vscode",
 		priority = 1000,
 		lazy = false,
 		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = {
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = true,
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = true,
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					telescope = true,
-					treesitter = true,
-				},
+			-- Set dark theme
+			vim.o.background = "dark"
+
+			require("vscode").setup({
+				-- Enable transparent background
+				transparent = true,
+
+				-- Enable italic comment
+				italic_comments = false,
+
+				-- Underline `@markup.link.*` variants
+				underline_links = true,
+
+				-- Disable nvim-tree background color
+				disable_nvimtree_bg = true,
+
+				-- Apply theme colors to terminal
+				terminal_colors = true,
 			})
 
-			vim.cmd("colorscheme catppuccin")
+			-- Load the theme
+			vim.cmd("colorscheme vscode")
 
 			-- Custom statusline for terminal buffers
 			vim.cmd("set laststatus=0") -- Disable default statusline
 			vim.cmd("set statusline=%{repeat('─',winwidth('.'))}") -- Custom statusline (only a line of dashes)
 
-			vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#45475a") -- GUI background transparent
-			vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#45475a") -- Inactive StatusLine transparent for GUI
+			vim.cmd("hi StatusLine gui=NONE guibg=NONE guifg=#444444") -- GUI background transparent
+			vim.cmd("hi StatusLineNC gui=NONE guibg=NONE guifg=#444444") -- Inactive StatusLine transparent for GUI
 		end,
 	},
 }
