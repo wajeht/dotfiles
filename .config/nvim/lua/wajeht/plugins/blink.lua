cf247f04cd08fc3fee5667572162cdc4033220ec
wajeht <58354193+wajeht@users.noreply.github.com>
2025-03-30 14:50:22 -0500

    style: Update code formatting in blink.lua and settings.json

--- .config/nvim/lua/wajeht/plugins/blink.lua
+++ .config/nvim/lua/wajeht/plugins/blink.lua
@@ -1,38 +1,9 @@
 return {
-  'saghen/blink.cmp',
-  enabled = true,
-  lazy = true,
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = '1.*',
-  opts = {
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-          menu = {
-            border = 'single',
-          },
-          documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 0,
-            window = { border = 'single' },
-          },
-          accept = {
-            auto_brackets = {
-              enabled = true,
-            },
-          },
-          ghost_text = {
-            enabled = true,
-          },
-      },
-      signature = {
-          enabled = true,
-          window = {
-            border = 'single',
-          },
-      },
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '1.*',
+    opts = {
       keymap = {
         preset = 'default',
         ["<C-k>"] = { "select_prev", "fallback" },
@@ -45,23 +16,25 @@ return {
         ["<CR>"] = { "accept", "fallback" },
         ["<Tab>"] = { "select_and_accept", "fallback" },
       },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+        },
+        -- ghost_text = { enabled = true },
+      },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+
       sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'buffer', 'snippets', 'path' },
       },
-      cmdline = {
-          completion = {
-              list = {
-                  selection = {
-                      preselect = false,
-                      auto_insert = true,
-                  },
-              },
-              menu = {
-                  auto_show = true,
-              },
-              ghost_text = { enabled = true },
-          },
-      },
+
+      signature = { enabled = true }
+    },
   },
-  opts_extend = { 'sources.default' },
 }
