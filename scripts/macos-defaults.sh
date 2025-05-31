84f63a15579e0c85bdd874fda7c6fd2718afac0b
wajeht <58354193+wajeht@users.noreply.github.com>
2025-05-31 18:27:32 -0500

    style: Enhance general UI/UX settings for macOS customization

--- scripts/macos-defaults.sh
+++ scripts/macos-defaults.sh
@@ -37,6 +37,22 @@ main() {
     info "Configuring menu bar..."
     set_default "com.apple.menuextra.battery" "ShowPercent" "string" "YES"
 
+    info "General UI/UX enhancements..."
+    set_default "NSGlobalDomain" "AppleShowScrollBars" "string" "Always"                # Always show scrollbars
+    set_default "NSGlobalDomain" "NSWindowResizeTime" "float" "0.001"                   # Increase window resize speed for Cocoa applications
+    set_default "NSGlobalDomain" "NSNavPanelExpandedStateForSaveMode" "bool" "true"     # Expand save panel by default
+    set_default "NSGlobalDomain" "NSNavPanelExpandedStateForSaveMode2" "bool" "true"    # Expand save panel by default
+    set_default "NSGlobalDomain" "PMPrintingExpandedStateForPrint" "bool" "true"        # Expand print panel by default
+    set_default "NSGlobalDomain" "PMPrintingExpandedStateForPrint2" "bool" "true"       # Expand print panel by default
+    set_default "NSGlobalDomain" "NSDocumentSaveNewDocumentsToCloud" "bool" "false"     # Save to disk (not to iCloud) by default
+    set_default "com.apple.LaunchServices" "LSQuarantine" "bool" "false"                # Disable the "Are you sure you want to open this application?" dialog
+    set_default "com.apple.systempreferences" "NSQuitAlwaysKeepsWindows" "bool" "false" # Disable Resume system-wide
+    set_default "NSGlobalDomain" "NSAutomaticCapitalizationEnabled" "bool" "false"      # Disable automatic capitalization
+    set_default "NSGlobalDomain" "NSAutomaticDashSubstitutionEnabled" "bool" "false"    # Disable smart dashes
+    set_default "NSGlobalDomain" "NSAutomaticPeriodSubstitutionEnabled" "bool" "false"  # Disable automatic period substitution
+    set_default "NSGlobalDomain" "NSAutomaticQuoteSubstitutionEnabled" "bool" "false"   # Disable smart quotes
+    set_default "NSGlobalDomain" "NSAutomaticSpellingCorrectionEnabled" "bool" "false"  # Disable auto-correct
+
     task "Restarting affected applications"
     killall Dock Finder SystemUIServer 2>/dev/null || true
 
