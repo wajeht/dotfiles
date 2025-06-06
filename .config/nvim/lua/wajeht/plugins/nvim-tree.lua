0ee46990caef210b5575b67bc5240f8acea9852e
wajeht <58354193+wajeht@users.noreply.github.com>
2025-06-06 10:51:40 -0500

    style: Update code formatting in nvim-tree.lua

--- .config/nvim/lua/wajeht/plugins/nvim-tree.lua
+++ .config/nvim/lua/wajeht/plugins/nvim-tree.lua
@@ -1,8 +1,6 @@
 return {
 	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- Optional, for file icons
-	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("nvim-tree").setup({
 			auto_reload_on_write = true,
