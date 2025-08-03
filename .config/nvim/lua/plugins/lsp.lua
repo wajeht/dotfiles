336c07a12e77dc8676173012d8decb0df40eff25
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-03 14:07:30 -0500

    refactor: simplify setup for lua language server in lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -116,25 +116,7 @@ return {
 
 		lspconfig["intelephense"].setup({ capabilities = capabilities, on_attach = on_attach })
 
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
+		lspconfig["lua_ls"].setup({ capabilities = capabilities, on_attach = on_attach })
 
 		vim.diagnostic.config({
 			virtual_text = true, -- Enable inline diagnostic messages
