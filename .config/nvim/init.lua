9aa34b66a8d31da0dcbe60819d45d2a290782ee3
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-03 15:51:15 -0500

    feat: lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
-require("config.options")
-require("config.autocmds")
-require("config.keymaps")
-require("config.terminal")
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
