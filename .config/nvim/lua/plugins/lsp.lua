35f126ee7417ce7995f1814460da69764aa8b313
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-03 22:29:20 -0500

    style: refactor vue language server path assignment for clarity

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -66,7 +66,8 @@ return {
 		lspconfig["html"].setup({ capabilities = capabilities, on_attach = on_attach })
 
 		-- Get the Vue Language Server path from Mason
-		local vue_language_server_path = vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+		local vue_language_server_path = vim.fn.stdpath("data")
+			.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 		local vue_plugin = {
 			name = "@vue/typescript-plugin",
 			location = vue_language_server_path,
