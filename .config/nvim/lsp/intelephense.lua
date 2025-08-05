8c932debbb3293a492bf9ab14483317b18e5233a
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-05 01:14:23 -0500

    chore(.config/nvim/lsp): update intelephense lsp configuration settings

--- .config/nvim/lsp/intelephense.lua
+++ .config/nvim/lsp/intelephense.lua
@@ -4,10 +4,44 @@ return {
 	root_markers = { "composer.json", "index.php", ".git" },
 	settings = {
 		intelephense = {
+			-- PHP stubs for better completions
+			stubs = {
+				"bcmath", "bz2", "calendar", "Core", "curl", "date",
+				"dom", "fileinfo", "filter", "gd", "gettext", "hash",
+				"iconv", "intl", "json", "libxml", "mbstring", "openssl",
+				"pcntl", "pcre", "PDO", "pdo_mysql", "pdo_sqlite", "Phar",
+				"posix", "readline", "Reflection", "session", "SimpleXML",
+				"soap", "sockets", "sodium", "SPL", "standard",
+				"superglobals", "tokenizer", "xml", "xmlreader",
+				"xmlwriter", "zip", "zlib"
+			},
+			-- File settings
 			files = {
-				excludePatterns = {
+				maxSize = 5000000,
+				associations = { "*.php", "*.phtml" },
+				exclude = {
 					"**/vendor/**",
+					"**/node_modules/**",
+					"**/.git/**",
+					"**/.svn/**",
+					"**/.hg/**",
+					"**/CVS/**",
+					"**/.DS_Store/**",
+					"**/bower_components/**"
 				}
+			},
+			-- Environment settings for better path resolution
+			environment = {
+				phpVersion = "8.2", -- Adjust to your PHP version
+			},
+			-- Diagnostics settings
+			diagnostics = {
+				enable = true,
+				run = "onType", -- or "onSave"
+			},
+			-- Format settings
+			format = {
+				enable = false -- Usually handled by prettier or other formatters
 			}
 		}
 	}
