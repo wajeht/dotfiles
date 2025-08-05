3643ff04e2ae795cab53718242292ccff2d6efde
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-05 00:54:46 -0500

    style(nvim): remove unnecessary intelephense settings in intelephense.lua

--- .config/nvim/lsp/intelephense.lua
+++ .config/nvim/lsp/intelephense.lua
@@ -2,13 +2,4 @@ return {
 	cmd = { "intelephense", "--stdio" },
 	filetypes = { "php" },
 	root_markers = { "composer.json", "index.php", ".git" },
-	settings = {
-		intelephense = {
-			hints = {
-				parameterNames = true,
-				parameterTypes = true,
-				typeHints = true,
-			},
-		},
-	},
 }
