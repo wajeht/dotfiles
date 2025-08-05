660966c1167ce142e81e872fc0d76fc85b5132e5
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-05 01:10:09 -0500

    style: update intelephense.lua settings for file exclusion patterns

--- .config/nvim/lsp/intelephense.lua
+++ .config/nvim/lsp/intelephense.lua
@@ -2,4 +2,13 @@ return {
 	cmd = { "intelephense", "--stdio" },
 	filetypes = { "php" },
 	root_markers = { "composer.json", "index.php", ".git" },
+	settings = {
+		intelephense = {
+			files = {
+				excludePatterns = {
+					"**/vendor/**",
+				}
+			}
+		}
+	}
 }
