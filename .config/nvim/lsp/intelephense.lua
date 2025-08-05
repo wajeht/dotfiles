f40577bca6850ceb4ceb87a9a06e6ef5fcfcfa26
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-05 13:05:34 -0500

    feat: add license

--- .config/nvim/lsp/intelephense.lua
+++ .config/nvim/lsp/intelephense.lua
@@ -1,8 +1,13 @@
 return {
-	cmd = { "intelephense", "--stdio" },
-	filetypes = { "php" },
-	root_markers = { "composer.json", "index.php", ".git" },
-	settings = {
-		intelephense = {},
-	},
+   cmd = { "intelephense", "--stdio" },
+   filetypes = { "php", "blade" },
+   root_markers = { "composer.json", "index.php", ".git" },
+   init_options = {
+       licenceKey = (function()
+           local f = assert(io.open(os.getenv("HOME") .. "/intelephense/license.txt", "rb"))
+           local content = f:read("*a")
+           f:close()
+           return string.gsub(content, "%s+", "")
+       end)(),
+   },
 }
