ef65e6f1ac71df1720c138ad15cc6abec2d0c4a5
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-06 20:09:17 -0500

    feat: add fzf-lua plugin and keymaps for file navigation and search

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -7,8 +7,4 @@ local autopairs = require("nvim-autopairs")
 autopairs.setup({
 	accept = { auto_brackets = { enabled = true } },
 	check_ts = true, -- enable treesitter
-	ts_config = {
-		lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-		javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-	},
 })
