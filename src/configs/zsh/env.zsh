4327ff207700fd7fef51ae61128f80b63146275c
wajeht <58354193+wajeht@users.noreply.github.com>
2025-11-16 13:53:09 -0600

    feat(zsh): support lazy-loading nvm from homebrew installation

--- src/configs/zsh/env.zsh
+++ src/configs/zsh/env.zsh
@@ -11,34 +11,57 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 # NVM (Node Version Manager) - Lazy loading for faster startup
-# Only load if NVM is installed
-if [ -d "$HOME/.nvm" ]; then
-  export NVM_DIR="$HOME/.nvm"
+# Check for Homebrew-installed nvm first, then ~/.nvm
+export NVM_DIR="$HOME/.nvm"
+if [[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]]; then
+  NVM_HOMEBREW="/opt/homebrew/opt/nvm"
+elif [[ -s "/usr/local/opt/nvm/nvm.sh" ]]; then
+  NVM_HOMEBREW="/usr/local/opt/nvm"
+fi
 
+# Only set up lazy loading if NVM is available
+if [[ -n "$NVM_HOMEBREW" ]] || [[ -d "$NVM_DIR" ]]; then
   # Lazy load NVM - only load when needed
   nvm() {
     unset -f nvm node npm npx
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Load nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Load nvm bash_completion
+    if [[ -n "$NVM_HOMEBREW" ]]; then
+      [ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
+      [ -s "$NVM_HOMEBREW/etc/bash_completion.d/nvm" ] && \. "$NVM_HOMEBREW/etc/bash_completion.d/nvm"
+    else
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    fi
     nvm "$@"
   }
 
   # Alias common node commands to trigger NVM loading
   node() {
     unset -f nvm node npm npx
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    if [[ -n "$NVM_HOMEBREW" ]]; then
+      [ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
+    else
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    fi
     node "$@"
   }
 
   npm() {
     unset -f nvm node npm npx
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    if [[ -n "$NVM_HOMEBREW" ]]; then
+      [ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
+    else
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    fi
     npm "$@"
   }
 
   npx() {
     unset -f nvm node npm npx
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    if [[ -n "$NVM_HOMEBREW" ]]; then
+      [ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
+    else
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    fi
     npx "$@"
   }
 fi
