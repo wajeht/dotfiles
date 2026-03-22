6c090f69b372ed6f5b9f3864fc36957346ab0658
wajeht <58354193+wajeht@users.noreply.github.com>
2026-03-22 14:18:11 -0500

    refactor: improve cd function for better directory navigation

--- src/configs/zsh/functions.zsh
+++ src/configs/zsh/functions.zsh
@@ -58,13 +58,12 @@ function importMDB() {
 # change dr and list them at same time
 function cd() {
   if [ $# -eq 0 ]; then
-    # No arguments - go to home directory
     builtin cd "$HOME" && lsd -lF
+  elif [ "$1" = "-" ]; then
+    builtin cd - && lsd -lF
   elif [ -d "$1" ]; then
-    # Valid directory - change to it
     builtin cd "$1" && lsd -lF
   else
-    # Invalid directory - show error and stay in current directory
     echo "cd: no such file or directory: $1"
     return 1
   fi
