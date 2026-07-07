d294c320c866296656573792f2915aef4d67b0cc
wajeht <58354193+wajeht@users.noreply.github.com>
2026-07-07 01:49:50 -0500

    fix(keys): route cmd+s through tmux as meta-s for save

--- src/configs/nvim/lua/config/keymaps.lua
+++ src/configs/nvim/lua/config/keymaps.lua
@@ -12,6 +12,8 @@ vim.keymap.set({ "n", "i", "v" }, "<D-/>", "gcc", { remap = true, desc = "Toggle
 
 -- Save and Select All
 vim.keymap.set({ "n", "v", "i" }, "<D-s>", "<cmd>w!<CR>", { desc = "Save file (force write)" }) -- Cmd+s in any mode
+-- Ghostty sends cmd+s as ESC s (<M-s>) so the key survives tmux
+vim.keymap.set({ "n", "v", "i" }, "<M-s>", "<cmd>w!<CR>", { desc = "Save file (force write)" })
 vim.keymap.set("n", "<D-a>", "ggVG", { desc = "Select all in normal mode" }) -- Cmd+a to select all text in normal mode
 vim.keymap.set("i", "<D-a>", "<Esc>ggVG", { desc = "Select all in insert mode" }) -- Cmd+a to select all text in insert mode
 vim.keymap.set("v", "<D-a>", "<Esc>ggVG", { desc = "Select all in visual mode" }) -- Cmd+a to select all text in visual mode
